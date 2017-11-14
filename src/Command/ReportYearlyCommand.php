78c90e9772dfe785d746da0ced8629549213f13e
Damjan Znidarsic <damjan.znidarsic@dlabs.si>
2017-11-14 10:16:17 +0000

    Initial commit

---
+++ src/Command/ReportYearlyCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace BOF\Command;
+
+use Doctrine\DBAL\Driver\Connection;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ReportYearlyCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('report:profiles:yearly')
+            ->setDescription('Page views report')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var $db Connection */
+        $io = new SymfonyStyle($input,$output);
+        $db = $this->getContainer()->get('database_connection');
+
+        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+
+        // Show data in a table - headers, data
+        $io->table(['Profile'], $profiles);
+
+    }
+}
