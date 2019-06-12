133cff5b5cc11687c52cc1d6cfc85c4420864132
Rok Vetrsek <rok.vetrsek@gmail.com>
2019-06-12 11:11:56 +0200

    Query the data and provide a basic output

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
 
 class ReportYearlyCommand extends ContainerAwareCommand
 {
@@ -13,20 +14,57 @@ class ReportYearlyCommand extends ContainerAwareCommand
     {
         $this
             ->setName('report:profiles:yearly')
-            ->setDescription('Page views report')
-        ;
+            ->setDescription('Page views report');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var $db Connection */
-        $io = new SymfonyStyle($input,$output);
-        $db = $this->getContainer()->get('database_connection');
+        $io = new SymfonyStyle($input, $output);
 
-        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+        $i = 0;
+
+        // Insist that the user provides a valid year
+        do {
+            if ($i > 0) {
+                $io->caution('The provided year is not a valid 4 digit intiger. Please try again.');
+            }
+            $year = intval($io->ask('For what year do you want the report?', date('Y')));
+            $i++;
+        } while (strlen($year) != 4);
+
+        // Query the database for views in the provided year
+        $db = $this->getContainer()->get('database_connection');
+        $sql = '
+            SELECT
+                P.profile_name,
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 1 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 2 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 3 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 4 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 5 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 6 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 7 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 8 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 9 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 10 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 11 THEN V.views ELSE 0 END), 0),
+                FORMAT(SUM(CASE WHEN MONTH(V.date) = 12 THEN V.views ELSE 0 END), 0)
+            FROM profiles P
+            LEFT JOIN views V ON V.profile_id = P.profile_id AND YEAR(V.date) = ' . $year . '
+            GROUP BY P.profile_id
+            ORDER BY P.profile_name
+            ';
+
+        $profiles = $db->query($sql)->fetchAll();
 
         // Show data in a table - headers, data
-        $io->table(['Profile'], $profiles);
+        $headers = ['Profile ' . $year,];
 
+        for ($i = 1; $i <= 12; $i++) {
+            array_push($headers, date('M', mktime(0, 0, 0, $i, 10)));
+        }
+
+        $io->table($headers, $profiles);
     }
 }
